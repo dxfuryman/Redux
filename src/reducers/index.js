8a93fbc3b6887f73277d3374b47b305c1c7bca80
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-05-22 17:47:35 +0530

    dev commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-//This file represents the root reducer,
-// which combines all the individual reducers in the application.
+//  yes//This file represents the root reducer,
+// // which combines all the individual reducers in the application.yes
 import taskReducer from './taskReducer.js'
 import {combineReducers} from "redux";
 
